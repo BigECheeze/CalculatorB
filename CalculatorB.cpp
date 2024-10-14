98d44b2c0f3fdf704e35451eff0aaaa4695d9ce3
rjlj2 <rjlj2@RJ-GALAXYBOOK3>
2024-10-14 16:20:39 -0500

    Add project files.

---
+++ CalculatorB.cpp
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+ CSCI 1101 - Calculator (Part B)
+
+ Team Coordinator: Robert Lopez
+
+ Collaborator #1: Elijah Martinez
+ Collaborator #2: Ceasar Martinez
+
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double multiply(double a, double b);
+double divide(double a, double b);
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (*, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    double result;
+
+    switch (operation) {
+    case '*':
+        result = multiply(num1, num2);
+        break;
+    case '/':
+        if (num2 != 0) {
+            result = divide(num1, num2);
+        }
+        else {
+            cerr << "Error: Division by zero!" << endl;
+            return 1;
+        }
+        break;
+
+    default:
+        cerr << "Error: Invalid operator!" << endl;
+        return 1;
+    }
+
+    cout << "Result: " << result << endl;
+    return 0;
+}
+
+
+// Feature 3: Multiplication.
+// Created by: [Collaborator #1]
+double multiply(double a, double b) {
+    return a*b;
+}
+
+// Feature 4: Division.
+// Created by: [Collaborator #2]
+double divide(double a, double b) {
+    return a/b;
+}
+
+
